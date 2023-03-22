4ef23c32a270353ab0fe77b9c9f4b2fb0661e827
niovi <prodamgarazhnedorogo@gmail.com>
2023-03-22 14:24:35 +0200

    ddb

--- core/settings.py
+++ core/settings.py
@@ -121,3 +121,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if DEBUG:
+    INSTALLED_APPS += ["debug_toolbar"]
+    MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware", ]
+    INTERNAL_IPS = ["127.0.0.1"]
