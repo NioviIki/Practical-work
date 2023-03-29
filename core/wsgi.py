6581429be7c1224b727cdda347e1fc19c6f60a89
niovi <prodamgarazhnedorogo@gmail.com>
2023-03-29 11:30:08 +0300

    fixtures

--- core/wsgi.py
+++ core/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+path = '/home/Niovi1/Practical-work'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'core.settings'
 
 application = get_wsgi_application()
